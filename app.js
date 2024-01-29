77c82e98dabeb7bfe45daedff80f510cbeeaca14
nipunModz <148088084+nipunModz@users.noreply.github.com>
2024-01-29 20:14:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// animat card ui //
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+  initialSlide: 2,
+  loop: true,
+  rotate: true,
+  mousewheel: {
+    invert: true,
+  },
+});
